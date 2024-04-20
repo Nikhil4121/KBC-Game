58e6f5d9f180cce556a118a1976510a295aae673
Nikhil <167554386+Nikhil4121@users.noreply.github.com>
2024-04-20 21:10:47 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,12 @@
+print ("WELCOME TO 2024 KBC SHOW")
+print("KBC GAME")
+name= input("Enter your name frist:")
+age = input("Enter your age:")
+print("Thank you for giving your detail.")
+e=[ a  b ]
+if e==[]:
+   print("who is nikk")
+else:
+   print("please select option one")
+   
+
